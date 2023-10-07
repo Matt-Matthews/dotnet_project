5bb56890df6ced70cea147dffc4ccd470b0aa7bf
Mathews Paku <pmathews@quickloc8.co.za>
2023-10-07 15:13:19 +0200

    Added the put class

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -6,7 +6,7 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-       
+
 
         [HttpGet] //api/activities
 
@@ -19,12 +19,21 @@ namespace API.Controllers
 
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await Mediator.Send(new Details.Query{Id = id});
+            return await Mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateActivity(Activity activity){
-            await Mediator.Send(new Create.Command{Activity = activity});
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command { Activity = activity });
             return Ok();
         }
     }
